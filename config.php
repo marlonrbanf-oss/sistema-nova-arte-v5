4def24af55b14f6351daa484e332a19c425361c9
marlonrbanf-oss <marlonrbanf@gmail.com>
2026-01-16 09:06:22 -0300

    Corrigindo URL do site no config.php

--- config.php
+++ config.php
@@ -1,13 +1,16 @@
-<?php 
+<?php
 
 $email_adm = 'marlonrbanf@gmail.com';
-$url_site = 'http://localhost/sites/Template%20Nova%20arte/Template%20Inicial/site';
 
+// Aqui está o segredo: Identificar se está no Railway ou no Computador
+if (getenv('MYSQLHOST')) {
+    // URL do seu site no Railway (substitua pelo seu link real do Railway)
+    $url_site = 'https://' . $_SERVER['HTTP_HOST'] . '/';
+} else {
+    // URL para quando você estiver testando no seu PC (Pop!_OS)
+    $url_site = 'http://localhost/sistema_nova_arte_v5/';
+}
 
-//DADOS PARA CONEXÃO COM BD LOCAL
-$banco = 'delivery';
-$host = 'localhost';
-$usuario = 'root';
-$senha = '';
-
- ?>
+// Para evitar conflitos, vamos usar as variáveis que já configuramos no conexao.php
+// Se você já tem o conexao.php, este arquivo config deve focar apenas em URLs e nomes.
+$nome_sistema = 'Nova Arte';
